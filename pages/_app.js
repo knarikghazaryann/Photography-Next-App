00c52c5faa01776ea78f357b7039012d6e2ab748
knarikghazaryann <knarikkazaryan777@gmail.com>
2023-01-22 22:22:22 +0400

    adding files

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import '@/styles/globals.css'
+import Navbar from '@/components/Navbar/Navbar';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  )
+
+
 }
